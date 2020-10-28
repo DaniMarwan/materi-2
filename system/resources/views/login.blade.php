384d028ae8d6080b73fb19e18c2390f4e5db1050
DaniMarwan <73591244+DaniMarwan@users.noreply.github.com>
2020-10-28 23:08:01 +0700

    Initial commit

---
+++ system/resources/views/login.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+      <div class="row justify-content-center mt-5">
+        <div class="col-md-4  bg-warning">
+          <div class="card">
+            <div class="card-header bg-transparent mb-0"><h5 class="text-center">Mohon Untuk <span class="font-weight-bold text-dark">LOGIN</span></h5></div>
+            <div class="card-body">
+              <form action="">
+                <div class="form-group">
+                  <input type="text" name="" class="form-control" placeholder="Masukan Email">
+                </div>
+                <div class="form-group">
+                  <input type="text" name="" class="form-control" placeholder="Masukan Sandi">
+                </div>
+                <div class="form-group custom-control custom-checkbox">
+                  <input type="checkbox" class="custom-control-input" id="customControlAutosizing">
+                  <label class="custom-control-label" for="customControlAutosizing">Ingatkan Sandi</label>
+                </div>
+                <div class="form-group">
+                  <input type="submit" name="" value="Masuk" class="btn btn-primary btn-block">
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
